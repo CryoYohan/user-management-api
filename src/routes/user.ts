7ca8bd0a043405a829889db5c1049375f5541213
unknown <YY428@ccs.local>
2025-03-28 04:54:17 -0700

    Transferring the code in creation for user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,5 @@
-
+//This is for the creation User()
+import { User } from "../entities/User";
 
 //get all users
 router.get('/users', async (req: Request, res: Response) => {
@@ -14,18 +15,26 @@ router.get('/users', async (req: Request, res: Response) => {
     }
 });
 
-router.get('/users/:id', async (req: Request, res: Response) => {
-    try {
-        const user : UnitUser | null = await database.findOne(req.params.id);
-        if (!user) {
-            res.status(StatusCodes.NOT_FOUND).json({ message: "User not found" });
-            return;
-        }
-        res.status(StatusCodes.OK).json(user);
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error", error });
-    }
-});
+router.post("/users", async (req: Request, res: Response) => {
+      const { name, email, password } = req.body;
+
+      try {
+        const userRepository = connection.getRepository(User);
+
+        // In production, ensure you hash the password before storing!
+        const user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = password;
+
+        const savedUser = await userRepository.save(user);
+
+        return res.status(201).json(savedUser);
+      } catch (error) {
+        console.error("Error creating user: ", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
 
 //deletion
 router.delete('/users/:id', async (req: Request, res: Response) => {
