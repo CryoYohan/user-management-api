d225287308577a0edb699beaba38a3423e6cbf28
itsmecyril <cyrilypil@gmail.com>
2025-03-29 12:41:38 +0800

    added error-handler, from sir gayo's, good practice

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,46 @@
-import "reflect-metadata";
-import express, { Request, Response } from 'express';
-import { AppDataSource, ensureDbExists } from "./dbhelper/db"
-import { userRouter } from './routes/user';
-import dotenv from "dotenv";
+import { AppDataSource, ensureDbExists } from "./_helpers/db"
+import express from 'express'
+import { userRouter } from './users/users.routes'
+import errorHandler from './_middlewares/error-handler'
+import * as dotevnv from "dotenv"
 
-const app = express();
-dotenv.config();
+const app = express()
+dotevnv.config();
 
-app.use(express.json());
+if (!process.env.PORT) {
+  console.log(`No port value specified...`);
+}
+const PORT = parseInt(process.env.PORT as string, 10);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+//db initilization
+ensureDbExists()
+  .then(() => {
+    AppDataSource.initialize()
+      .then(() => {
+        console.log("Database connected")
+      })
+      .catch(error => console.log(error))
+  })
+
+// Routes
+app.use('/api/users', userRouter);
+
+// Catch 404 errors and forward to error handler
+app.use((req, res, next) => {
+  next("Not Found");
 });
+
+// Global error handler (must be last!)
+app.use(errorHandler);
+
+
+//start server
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`)
+})
